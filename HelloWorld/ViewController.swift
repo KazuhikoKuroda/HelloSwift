905d8bb520bf7572fe75fd3dd0c33c85cd4dc029
KazuhikoKuroda <kuroda2395@gmail.com>
2015-11-05 13:30:36 +0900

    文字列操作系

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -61,13 +61,113 @@ class ViewController: UIViewController {
         println(checkSize(obj1))
         println(checkSize(obj2))
         println(checkSize(obj3))
+        
+        let metro = ["M": "丸ノ内線", "H": "日比谷線", "G": "銀座線", "T": "東西線"]
+        for (key, value) in metro {
+            println("\(key)は\(value)です")
+        }
+        for key in metro.keys {
+            println("\(key)どぇす")
+        }
+        for value in metro.values {
+            println("\(value)どぇす")
+        }
+
+        var ticket=5, power=30
+        while (ticket > 0) && (power < 100) {
+            ticket--
+            power += 20
+        }
+        println("\(power)、残チケット\(ticket)")
+
+        var a:UInt32, b:UInt32, c:UInt32, total:Int
+        do {
+            a = arc4random_uniform(13)+1
+            b = arc4random_uniform(13)+1
+            c = arc4random_uniform(13)+1
+            total = Int(a + b + c)
+        } while (total != 21)
+        println("\(a),\(b),\(c)")
+
+        xloop: for x in 0...5 {
+            yloop: for y in 0...5 {
+                if (x < y) {
+                    print("\n")
+                    continue xloop
+                }
+                print((x, y))
+            }
+        }
+        print("\n")
+        print("\n")
+
+        xloop: for x in 0...5 {
+            yloop: for y in 0...5 {
+                if (x > 3) {
+                    break xloop
+                }
+                print((x, y))
+            }
+            print("\n")
+        }
+        
+        let stars = String(count:10, repeatedValue:Character("⭐️"))
+        println(stars + String(count(stars)))
+
+        let str1 = "SWIFT", str2 = "swift"
+        if (str1.lowercaseString == str2.lowercaseString) {
+            println("\(str1)と\(str2)は大小文字問わなければ同じ")
+        }
+
+        let tanka:String = "1000", kosu:String = "5"
+        let kingaku:Int = tanka.toInt()! * kosu.toInt()!
+        println(kingaku)
+
+        let r = 120, pai = "3.14"
+        let ensyu:Float = Float(2 * r) * (pai as NSString).floatValue
+        println("円周は\(ensyu)です")
+        
+        let weight = 135.2, package = 10.5
+        println("内容量" + weight.description + "グラム\nパッケージ含めると" +
+            (weight + package).description + "グラム")
+
+        let str3 = "あいうえおかきくけこ"
+        let range:Range? = str3.rangeOfString("えおか")
+        if let theRange = range {
+            println("「\(str3)」の\(range!)は「\(str3[theRange])」")
+        } else {
+            println("見つかりません")
+        }
+
+        let address = "神奈川県茅ヶ崎市東海岸"
+        let findindex = find(address, "県")
+        if findindex != nil {
+            let address1 = address[address.startIndex...findindex!]
+            println("address1:\(address1)")
+            var start2 = findindex?.successor()
+            var end2 = address.endIndex
+            let address2 = address[start2!..<end2]
+            println("address2:\(address2)")
+        }
+
+        if address.hasPrefix("神奈川") && address.hasSuffix("東海岸") {
+            println("神奈川ではじまり東海岸でおわる")
+        }
+
+        let start = advance(address.startIndex, 4)
+        let end = advance(start, 4)
+        let pickupaddress = address[start..<end]
+        println(pickupaddress)
+
+        var pickupaddress2 = (address as NSString).substringFromIndex(4)
+        pickupaddress2 = (pickupaddress2 as NSString).substringToIndex(4)
+        println(pickupaddress2)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
     func checkSize(size:(width:Int, height:Int, weight:Int)) -> String {
         var ret = ""
